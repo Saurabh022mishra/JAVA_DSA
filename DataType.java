2bf8d99c90c8c72d570fb6647270bc2e8344cf8a
Saurabh022mishra <skm80050@gmail.com>
2024-09-14 14:57:02 +0530

    DataType

---
+++ DataType.java
@@ -0,0 +1,16 @@
+public class DataType {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 10.5f;
+        double c = 10.5;
+        char d = 'A';
+        boolean e = true;
+        String f = "Hello Java";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}

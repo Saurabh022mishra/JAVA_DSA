bea7ba7eb0c50ac4bfc3d512db7c080df368b5fc
Saurabh022mishra <skm80050@gmail.com>
2024-09-14 14:32:01 +0530

    Output

---
+++ output.java
@@ -0,0 +1,7 @@
+public class output {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.print("Hello Java");
+        System.out.println("Hello \n Java"); 
+    }
+}

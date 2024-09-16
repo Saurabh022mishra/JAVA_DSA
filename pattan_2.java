3321ec36acffa4736d278b3c348e7a5ac13eaee1
Saurabh022mishra <skm80050@gmail.com>
2024-09-16 22:47:09 +0530

    Pattern

---
+++ pattan_2.java
@@ -0,0 +1,23 @@
+/*
+ *****
+ *****
+ *****
+ *****
+ *****
+ 
+ */
+import java.util.Scanner;
+public class pattan_2 {
+    public static void main(String[] args) {
+        System.out.println("Enter Number of rows");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0; i<=n;i++){
+            for(int j=0;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println("5");
+        }
+    }
+    
+}
